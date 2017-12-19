91fcfbd2250f0098ea7b176ac21bb3ec32d58754
Mihai Toader <mihai.toader@catalysts.cc>
2017-12-19 17:21:01 +0200

    Some test

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by mtoader on 12/18/2017.
  */
+$(document).ready(() => {
+    $.ajax({url: "https://www.cryptocompare.com/api/data/coinlist/", success: function(result){
+        console.log(result);
+    }});
+});
